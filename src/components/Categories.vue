eedd743729ea7a74a7c5d7e2394dc9294e9b2a88
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-31 09:54:43 +0700

    .

---
+++ src/components/Categories.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="categories">
+    <div class="container">
+      <div class="row justify-content-center align-items-center mt-4 mb-5">
+        <div class="man_model col-6"></div>
+        <div class="girl_model col-6"></div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CategoriesComponent'
+};
+</script>
+
+<style>
+.man_model {
+  background: url('../assets/images/man_model.jpg');
+  background-size: cover;
+  height: 50vh;
+}
+
+.girl_model {
+  background: url('../assets/images/girl_model.jpg');
+  background-size: cover;
+  height: 50vh;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <template>
   <main>
     <Hero />
+    <Categories />
   </main>
 </template>
 
 <script>
 import Hero from '../components/Hero.vue';
+import Categories from '../components/Categories.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    Hero
+    Hero,
+    Categories
   }
 };
 </script>
