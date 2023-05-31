d6976ca0eefbbd618c1261c108a2af33d578fa40
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-31 21:10:27 +0700

    .

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="categories">
     <div class="container">
-      <div class="row justify-content-center align-items-center mt-4 mb-5 gap-lg-0 gap-md-3 gap-3">
+      <div class="row justify-content-center align-items-center mt-4 mb-5 gap-lg-0 gap-md-3 gap-2">
         <div class="man_model col-lg-6 position-relative">
           <div class="label position-absolute bottom-0 ms-2">
             <h3 class="text-light fw-bold">Man's Fashions</h3>
