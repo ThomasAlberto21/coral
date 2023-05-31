b4dbbe940af98a8a9577390920b0c4765a72f4ab
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-31 10:03:14 +0700

    .

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,9 +1,19 @@
 <template>
   <section class="categories">
     <div class="container">
-      <div class="row justify-content-center align-items-center mt-4 mb-5">
-        <div class="man_model col-6"></div>
-        <div class="girl_model col-6"></div>
+      <div class="row justify-content-center align-items-center mt-4 mb-5 gap-lg-0 gap-md-3 gap-3">
+        <div class="man_model col-lg-6 position-relative">
+          <div class="label position-absolute bottom-0 ms-2">
+            <h3 class="text-light fw-bold">Man's atributes</h3>
+            <p class="text-light">20 Items</p>
+          </div>
+        </div>
+        <div class="girl_model col-lg-6 position-relative">
+          <div class="label position-absolute bottom-0 ms-2">
+            <h3 class="text-light fw-bold">Woman's atributes</h3>
+            <p class="text-light">20 Items</p>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -19,12 +29,22 @@ export default {
 .man_model {
   background: url('../assets/images/man_model.jpg');
   background-size: cover;
-  height: 50vh;
+  height: 45vh;
+}
+
+.man_model:hover {
+  opacity: 0.7;
+  cursor: pointer;
 }
 
 .girl_model {
   background: url('../assets/images/girl_model.jpg');
   background-size: cover;
-  height: 50vh;
+  height: 45vh;
+}
+
+.girl_model:hover {
+  opacity: 0.7;
+  cursor: pointer;
 }
 </style>
