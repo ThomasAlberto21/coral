1103642298da0405c6779e480f659c0a2784854a
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-31 20:45:00 +0700

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,14 +4,13 @@
     <Categories />
 
     <div class="container">
-      <div class="row">
-        
-      </div>
+      <div class="row"></div>
     </div>
   </main>
 </template>
 
 <script>
+import axios from 'axios';
 import Hero from '../components/Hero.vue';
 import Categories from '../components/Categories.vue';
 
@@ -20,6 +19,33 @@ export default {
   components: {
     Hero,
     Categories
+  },
+
+  data() {
+    return {
+      trending_products: []
+    };
+  },
+
+  methods: {
+    setTrendingProducts(data) {
+      this.trending_products = data;
+    },
+
+    getTrendingProducts() {
+      axios
+        .get('  http://localhost:3000/trending_products')
+        .then((response) => {
+          this.setTrendingProducts(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  },
+
+  mounted() {
+    this.getTrendingProducts();
   }
 };
 </script>
