2a85b713432a44488d344828fb81d4d236ffbb51
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-06-01 20:05:35 +0700

    .

--- src/views/AllProductsView.vue
+++ src/views/AllProductsView.vue
@@ -1,6 +1,28 @@
 <template>
   <section class="all_products">
-    <h1>All Products</h1>
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col-6">
+          <h3 class="fw-semibold">All Products</h3>
+        </div>
+
+        <div class="col-6">
+          <div class="input-group">
+            <input
+              v-model="search"
+              type="text"
+              class="form-control"
+              placeholder="Search Products..."
+              aria-label="Search Plants..."
+              aria-describedby="basic-addon2"
+            />
+            <div class="input-group-text bg-success opacity-75" id="search">
+              <i class="bi bi-search text-light"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
