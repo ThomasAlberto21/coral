5ff97a396ebd622f84074b0fa1fbb8701ef08c57
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-06-02 10:39:27 +0700

    .

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
-import AboutView from '../views/AboutView.vue';
+import Contact from '../views/ContactView.vue';
 import AllProductsView from '../views/AllProductsView.vue';
 
 const router = createRouter({
@@ -12,14 +12,14 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'AboutView',
-      component: AboutView
-    },
-    {
       path: '/all_products',
       name: 'AllProductsView',
       component: AllProductsView
+    },
+    {
+      path: '/contact',
+      name: 'ContactView',
+      component: Contact
     }
   ]
 });
