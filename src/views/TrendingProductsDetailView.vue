342cf513e6711b91ecf62a1151e5788b9718c8c5
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-06-02 11:51:37 +0700

    .

--- src/views/TrendingProductsDetailView.vue
+++ src/views/TrendingProductsDetailView.vue
@@ -2,18 +2,32 @@
   <section class="trending_products_detail">
     <div class="container my-5">
       <div class="row">
-        <div class="col-lg-5 col-12">
-          <img
-            :src="products.image"
-            alt="best_products_image"
-            class="rounded-4 img-fluid w-100 h-100"
-          />
+        <div class="col-lg-6 col-12">
+          <div class="main_image">
+            <img :src="products.image" class="rounded-4 img-fluid w-100" />
+          </div>
+
+          <div class="thumbnails_image d-flex w-100 h-25 gap-2 mt-5">
+            <img
+              v-for="(image, index) in products.thumbnails"
+              :key="index"
+              class="img-fluid w-50 h-75 card"
+              :src="image"
+              @click="changeImage(index)"
+            />
+          </div>
         </div>
 
-        <div class="col-lg-7">
+        <div class="col-lg-6 px-4">
           <h3 class="fw-bold">{{ products.title }}</h3>
           <hr />
-          <h4 class="fw-semibold mb-3">${{ products.price }}</h4>
+          <div class="d-flex justify-content-between align-items-center">
+            <h4 class="fw-semibold mb-3">${{ products.price }}</h4>
+            <p class="fw-semibold text-warning">
+              <i class="bi bi-star-fill"></i> {{ products.rating }} / 5
+            </p>
+          </div>
+
           <p class="fw-normal">{{ products.description }}</p>
         </div>
       </div>
@@ -29,13 +43,18 @@ export default {
 
   data() {
     return {
-      products: {}
+      products: {},
+      currentImage: ''
     };
   },
 
   methods: {
     setProducts(data) {
       this.products = data;
+    },
+
+    changeImage(index) {
+      this.products.image = this.products.thumbnails[index];
     },
 
     fetchDataTrendingProducts() {
@@ -55,3 +74,7 @@ export default {
   }
 };
 </script>
+
+
+
+<style></style>
