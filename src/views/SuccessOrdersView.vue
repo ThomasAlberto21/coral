5ba2d3e2ccf70eceb6e3c6c3943baf6cd8444cae
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-06-03 21:35:43 +0700

    .

--- src/views/SuccessOrdersView.vue
+++ src/views/SuccessOrdersView.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>
-    <h1>success_order</h1>
-  </div>
+  <section class="succres_orders">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col text-center">
+          <img
+            src="../assets/images/success_order.png"
+            alt="success_order_img"
+            class="img-fluid w-50"
+          />
+          <h1 class="fw-bold mt-5">Success Order</h1>
+          <p>Your order will be delivered soon, please wait</p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
